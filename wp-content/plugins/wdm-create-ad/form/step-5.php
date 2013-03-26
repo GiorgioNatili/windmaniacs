99898bd44331b9c7faa38ebef358e34183834010
mavimo <mavimo@gmail.com>
2013-03-26 08:41:22 +0100

    Added message to notify user the content creation

--- wp-content/plugins/wdm-create-ad/form/step-5.php
+++ wp-content/plugins/wdm-create-ad/form/step-5.php
@@ -1,5 +1,5 @@
 <?php if (is_user_logged_in()): ?>
-  <?php wp_redirect('http://www.google.com/'); exit; ?>
+  <h2><?php print _('Congratulation, your annunce will be publish as fast as possible.') ?></h2>
 <?php else: ?>
   <h2><a href="<?php print site_url('wp-login.php?redirect_to=' . $destination_url, 'login') ?>"><?php print _('Already a windmaniacs'); ?></a></h2>
   <h2><a href="<?php print site_url('wp-login.php?action=register&redirect_to=' . $destination_url, 'login') ?>"><?php print _('Still not a windmaniac'); ?></a></h2>
