2b34ce4bbb85b40ff99a89ff23771e4b76d140e7
mavimo <mavimo@gmail.com>
2013-03-26 08:42:33 +0100

    Add some JS to manage ajax interaction

--- wp-content/plugins/wdm-create-ad/js/wdm-create-ad.js
+++ wp-content/plugins/wdm-create-ad/js/wdm-create-ad.js
@@ -1,3 +1,27 @@
 ;(function ($) {
-  console.log('DM create init');
+  var form_name = 'form.wdm-create-ad';
+
+  var startPreloader = function () {
+    console.log('Preloader START');
+  };
+
+  var stopPreloader = function () {
+    console.log('Preloader STOP');
+  };
+
+  $(form_name).submit( function () {
+    $.ajax({
+      type       :'post',
+      url        : '',
+      data       : $(form_name).serialize(),
+      beforeSend : startPreloader,
+      complete   : stopPreloader,
+      success    : function (result) {
+        var form = $(form_name, result);
+        console.log(form[0]);
+        $(form_name).replaceWith(form[0]);
+      }
+    });
+    return false;
+  });
 })(jQuery);
