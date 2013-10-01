a7d6d98c9046bd8356aca4fa18b0a171419e0825
Arumugam <bsubramanian.a@gmail.com>
2013-10-01 01:46:42 -0700

    social notify page

--- wp-content/themes/yourflexishop/header.php
+++ wp-content/themes/yourflexishop/header.php
@@ -49,6 +49,7 @@ jQuery( this ).dialog( "close" );
 <?php if(prima_get_option('usernav')&&class_exists('WP_eCommerce')) get_template_part( 'flexi-usernav' ); ?>
 <div id="flexi-wrapper">
   <div id="shadow">
+  <div id="bf"></div>
 <?php
   echo "allow";
    $social_notify = $_GET["social_notify"];
