5493f3ba49c12f222608a86aa6a1bc5f7223da71
Arumugam <bsubramanian.a@gmail.com>
2013-10-07 07:32:06 -0700

    language issue

--- wp-content/plugins/wdm-create-ad/form/form-wrapper.php
+++ wp-content/plugins/wdm-create-ad/form/form-wrapper.php
@@ -3,20 +3,20 @@
 <div class="formcarbonbackground">
   <div id="stepnavbar">
     <ul>
-      <li id="step1" class="<?php print ($step == 1) ? 'active' : 'off'; ?>"><span class="step">Step n&deg;1</span><br />
-        <span class="descriptionstep">Scegli la categoria</span>
+      <li id="step1" class="<?php print ($step == 1) ? 'active' : 'off'; ?>"><span class="step"> <?php _e('Step', 'qtranslate'); ?>n&deg;1</span><br />
+        <span class="descriptionstep"> <?php _e('Choose the category', 'qtranslate'); ?></span>
       </li>
-      <li id="step2" class="<?php print ($step == 2) ? 'active' : 'off'; ?>"><span class="step">Step n&deg;2</span><br />
-        <span class="descriptionstep">Seleziona una o pi&ugrave; sottocategorie e <br />inserisci i dati dell'annuncio</span>
+      <li id="step2" class="<?php print ($step == 2) ? 'active' : 'off'; ?>"><span class="step"> <?php _e('Step', 'qtranslate'); ?> n&deg;2</span><br />
+        <span class="descriptionstep"><?php _e('Select one or more subcategories and enter the details of the announcement', 'qtranslate'); ?></span>
       </li>
-      <li id="step3" class="<?php print ($step == 3) ? 'active' : 'off'; ?>"><span class="step">Step n&deg;3</span><br />
-        <span class="descriptionstep">Dichiara lo stato d'uso <br />del tuo prodotto in vendita</span>
+      <li id="step3" class="<?php print ($step == 3) ? 'active' : 'off'; ?>"><span class="step"><?php _e('Step', 'qtranslate'); ?> n&deg;3</span><br />
+        <span class="descriptionstep"><?php _e('Declares a state of use', 'qtranslate'); ?> <br /><?php _e('Your product for sale', 'qtranslate'); ?></span>
       </li>
-      <li id="step4" class="<?php print ($step == 4) ? 'active' : 'off'; ?>"><span class="step">Step n&deg;4</span><br />
-        <span class="descriptionstep">Carica al massimo <br />5 immagini</span>
+      <li id="step4" class="<?php print ($step == 4) ? 'active' : 'off'; ?>"><span class="step"><?php _e('Step', 'qtranslate'); ?> n&deg;4</span><br />
+        <span class="descriptionstep"><?php _e('Upload up to 5 images', 'qtranslate'); ?></span>
       </li>
-      <li id="step5" class="<?php print ($step == 5) ? 'active' : 'off'; ?>"><span class="step">Step n&deg;5</span><br />
-        <span class="descriptionstep">Pubblica l'annuncio</span>
+      <li id="step5" class="<?php print ($step == 5) ? 'active' : 'off'; ?>"><span class="step"><?php _e('Step', 'qtranslate'); ?> n&deg;5</span><br />
+        <span class="descriptionstep"><?php _e('Public announcement', 'qtranslate'); ?></span>
       </li>
     </ul>
   </div>
@@ -31,12 +31,12 @@
   </div>
 
   <?php if ($step > 1 && $step  < 5): ?>
-    <input type="submit" name="action_prev" class="downstepnavigator"  value="<?php print _('Step') . ' ' . ($step - 1); ?>" /></button>
+    <input type="submit" name="action_prev" class="downstepnavigator"  value="<?php print _e('Step', 'qtranslate') . ' ' . ($step - 1); ?>" /></button>
   <?php endif; ?>
   <?php if ($button_current): ?>
     <input type="submit" name="submit" class="currentstepnavigator" value="Submit" />
   <?php endif; ?>
   <?php if ($step < 5): ?>
-    <input type="submit" class="upstepnavigator" name="action_next"   value="<?php print _('Step') . ' ' . ($step + 1); ?>"></button>
+    <input type="submit" class="upstepnavigator" name="action_next"   value="<?php print _e('Step', 'qtranslate') . ' ' . ($step + 1); ?>"></button>
   <?php endif; ?>
 </form>
