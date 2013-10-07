63a67cfcf6d72686b6aa41e79c77f3c4c14b2285
Arumugam <bsubramanian.a@gmail.com>
2013-10-07 07:34:55 -0700

    language issue

--- wp-content/plugins/wdm-create-ad/form/step-4.php
+++ wp-content/plugins/wdm-create-ad/form/step-4.php
@@ -1,5 +1,5 @@
 <div id="dynamic-upload-images">
-    Caricamento immagini (Max 5, Max 1Mb, jpg gif png format) <br />
+    <?php _e('Loading images (Max 5, Max 1Mb, jpg gif png format)', 'qtranslate'); ?> <br />
     <?php for($i = 0; $i < 5; $i++): ?>
 	<input type="file" name="uploadfiles_<?php print $i; ?>" value="SELEZIONA FILE DAL DISPOSITIVO" class="uploadfiles" />  
     <?php endfor; ?>
