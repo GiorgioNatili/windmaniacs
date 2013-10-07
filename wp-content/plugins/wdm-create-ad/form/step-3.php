7fdb478cdcb3c84d20725bc75525618575888525
Arumugam <bsubramanian.a@gmail.com>
2013-10-07 07:34:10 -0700

    language issue

--- wp-content/plugins/wdm-create-ad/form/step-3.php
+++ wp-content/plugins/wdm-create-ad/form/step-3.php
@@ -1,8 +1,8 @@
 <div id="stepcondition">
   <div id="selectcondition" class="select">
-    <label for="wdm-condiction"><?php _e("Please select Condiction"); ?></label>
+    <label for="wdm-condiction"><?php _e("Please select Condiction", 'qtranslate'); ?></label>
     <select name="wdm-condiction">
-      <option value=""><?php _e('Condition'); ?></option>
+      <option value=""><?php _e('Condition', 'qtranslate'); ?></option>
     <?php foreach ($condictions as $value => $label):?>
       <option value="<?php print $value; ?>" <?php print wdm_create_ad_is_default($value, $default_value['wdm-condiction']) ?>><?php print $label; ?></option>
     <?php endforeach?>
@@ -10,9 +10,9 @@
   </div>
 
   <div id="selectyear" class="select">
-    <label for="wdm-year"><?php _e("Year"); ?></label>
+    <label for="wdm-year"><?php _e("Year", 'qtranslate'); ?></label>
     <select name="wdm-year">
-      <option value=""><?php _e('Please select Year'); ?></option>
+      <option value=""><?php _e('Please select Year', 'qtranslate'); ?></option>
     <?php foreach ($years as $value => $label):?>
       <option value="<?php print $value; ?>" <?php print wdm_create_ad_is_default($value, $default_value['wdm-year']) ?>><?php print $label; ?></option>
     <?php endforeach?>
@@ -20,9 +20,9 @@
   </div>
 
   <div id="selectstyle" class="select">
-    <label for="wdm-style"><?php _e("Style"); ?></label>
+    <label for="wdm-style"><?php _e("Style", 'qtranslate'); ?></label>
     <select name="wdm-style">
-      <option value=""><?php _e('Please select style'); ?></option>
+      <option value=""><?php _e('Please select style', 'qtranslate'); ?></option>
     <?php foreach ($styles as $value => $label):?>
       <option value="<?php print $value; ?>" <?php print wdm_create_ad_is_default($value, $default_value['wdm-style']) ?>><?php print $label; ?></option>
     <?php endforeach?>
